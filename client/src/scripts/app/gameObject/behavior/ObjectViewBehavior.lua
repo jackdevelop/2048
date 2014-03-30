5d40f4d74ebb729f6f22d99ec4c1dd386fb9c57a
jackdevelop <jackdevelop@sina.com>
2014-03-30 13:10:57 +0800

    test

--- client/src/scripts/app/gameObject/behavior/ObjectViewBehavior.lua
+++ client/src/scripts/app/gameObject/behavior/ObjectViewBehavior.lua
@@ -149,6 +149,7 @@ function ObjectViewBehavior:bind(object)
 --	        transition.moveTo(object.sprite1_, {x=10   ,y = 500,time = 0.2 })
 	    else
 	    	object.sprite_:setButtonLabelString(toint(number))
+	    	object.sprite_:set
 	    end
 	 end
 	 object:bindMethod(self, "setButtonLabel", setButtonLabel)
